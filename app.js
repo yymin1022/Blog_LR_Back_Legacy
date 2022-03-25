5e6537b787f2c995378e52020acd0fa4b4b461ba
yymin1022 <yymin1022@gmail.com>
2022-03-25 19:55:20 +0900

    Initialized Basic Code

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+var server = http.createServer(app);
+
+app.get("/", function(req, res){
+    res.send("root page");
+});
+
+app.get("/start", function(req, res){
+    res.send("start page");
+});
+
+server.listen(8080, "0.0.0.0", function(){
+    console.log("Server listen on port " + server.address().port);
+});
