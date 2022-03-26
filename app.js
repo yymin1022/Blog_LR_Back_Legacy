dbb7d8bc1b2cb09840dfd60871f574e4179f1efc
yymin1022 <yymin1022@gmail.com>
2022-03-26 16:04:46 +0900

    Enabled .env Variable

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var http = require("http");
 var app = express();
 var server = http.createServer(app);
 
+require('dotenv').config();
+
 app.get("/", function(req, res){
     res.redirect("https://dev-lr.com");
 });
 
-app.get("/start", function(req, res){
-    res.send("start page");
+app.get("/getPostList", function(req, res){
+    res.send("Get Post List");
 });
 
 server.listen(8080, "0.0.0.0", function(){
